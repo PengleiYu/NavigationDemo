6706739354ec9c440bfff1e7f4ca8f3e5c02bcef
penglei <yupengniaosanshi@gmail.com>
2019-08-16 00:02:03 +0800

    main跳转profile

--- app/src/main/java/com/example/navigatordemo/login/MainFragment.kt
+++ app/src/main/java/com/example/navigatordemo/login/MainFragment.kt
@@ -2,12 +2,13 @@ package com.example.navigatordemo.login
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.navigatordemo.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +22,13 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_goProfile_main.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_profileFragment)
+        }
     }
 
 
